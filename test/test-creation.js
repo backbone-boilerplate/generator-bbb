9e9616ca4a57a20c1cb1e2b92f7be2374f90df3f
Simon Boudrias <admin@simonboudrias.com>
2013-07-24 21:21:18 -0400

    basic tests (starting point)

--- test/test-creation.js
+++ test/test-creation.js
@@ -1,34 +1,39 @@
 /*global describe, beforeEach, it*/
-'use strict';
+"use strict";
 
-var path    = require('path');
-var helpers = require('yeoman-generator').test;
+var path    = require("path");
+var helpers = require("yeoman-generator").test;
 
 
-describe('bbb generator', function () {
+describe("bbb generator", function () {
   beforeEach(function (done) {
-    helpers.testDirectory(path.join(__dirname, 'temp'), function (err) {
+    helpers.testDirectory(path.join(__dirname, "temp"), function (err) {
       if (err) {
         return done(err);
       }
 
-      this.app = helpers.createGenerator('bbb', [
-        '../../app'
+      this.app = helpers.createGenerator("bbb:app", [
+        "../../lib/generators/app"
       ]);
+      this.app.options["skip-install"] = true;
       done();
     }.bind(this));
   });
 
-  it('creates expected files', function (done) {
+  it("creates expected files", function (done) {
     var expected = [
       // add files you expect to exist here.
-      '.jshintrc',
-      '.editorconfig'
+      ".bbb-rc.json",
+      ".bowerrc",
+      "bower.json",
+      "Gruntfile.js",
+      "index.html",
+      "app/config.js",
+      "app/app.js",
+      "app/main.js"
     ];
 
-    helpers.mockPrompt(this.app, {
-      'someOption': 'Y'
-    });
+    helpers.mockPrompt(this.app);
 
     this.app.run({}, function () {
       helpers.assertFiles(expected);
--- test/test-load.js
+++ test/test-load.js
@@ -1,11 +1,11 @@
 /*global describe, beforeEach, it*/
-'use strict';
+"use strict";
 
-var assert  = require('assert');
+var assert  = require("assert");
 
-describe('bbb generator', function () {
-  it('can be imported without blowing up', function () {
-    var app = require('../app');
+describe("bbb generator", function () {
+  it("can be imported without blowing up", function () {
+    var app = require("../lib/generators/app");
     assert(app !== undefined);
   });
 });
