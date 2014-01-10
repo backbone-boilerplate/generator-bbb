c70357d2725fe0d818aa1bb38558d81ab05ccde7
Simon Boudrias <simon.boudrias@cossette.com>
2014-01-10 16:59:03 -0500

    Add tests and fill in Bower.json version and name

--- test/test-creation.js
+++ test/test-creation.js
@@ -1,6 +1,8 @@
-var path    = require("path");
+var path = require("path");
+var fs = require("fs");
 var helpers = require("yeoman-generator").test;
 var assert = require("yeoman-generator").assert;
+var detectIndent = require("detect-indent");
 
 var defaultFiles = [
   ".bowerrc",
@@ -36,6 +38,34 @@ describe("bbb generator", function () {
         "test/qunit/specs/example.spec.js",
         "test/qunit/specs/boilerplate/router.spec.js"
       ]));
+    });
+
+    it("indent files with 2 spaces", function () {
+      var file = fs.readFileSync("app/app.js", "utf8");
+      assert.equal(detectIndent(file), "  ");
+    });
+  });
+
+  describe("specifying project name", function () {
+    before(function (done) {
+      helpers.testDirectory(path.join(__dirname, "temp"), function() {
+        this.app = helpers.createGenerator("bbb:app", [
+          path.join(__dirname, "../lib/generators/app")
+        ]);
+        this.app.options["skip-install"] = true;
+        helpers.mockPrompt(this.app, { name: "bbb-project" });
+        this.app.run({}, function () { done(); });
+      }.bind(this));
+    });
+
+    it('fill in package.json', function () {
+      var pkg = JSON.parse(fs.readFileSync("package.json", "utf8"));
+      assert.equal(pkg.name, "bbb-project");
+    });
+
+    it('fill in bower.json', function () {
+      var pkg = JSON.parse(fs.readFileSync("bower.json", "utf8"));
+      assert.equal(pkg.name, "bbb-project");
     });
   });
 });
